53c2edd886d56b9d79f559ac8ee7eeb3f32cb8c0
Mansoor Amza <113126176+MannyAmza@users.noreply.github.com>
2023-12-18 15:25:21 -0800

    Update Button.jsx

--- recipe-builder/src/components/Button.jsx
+++ recipe-builder/src/components/Button.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+/**
+ * Button component to be used throughout the application.
+ * @param {boolean} isDisabled - Indicates if the button should be disabled.
+ * @param {string} btnType - Specifies the type of the button (e.g., "button", "submit").
+ * @param {string} containerStyle - Additional CSS classes for the button container.
+ * @param {string} textStyle - Additional CSS classes for the button text.
+ * @param {string} title - The text content of the button.
+ * @param {React.Element} rightIcon - The optional icon to be displayed on the right side of the button.
+ * @param {function} handleClick - The function to be executed when the button is clicked.
+ */
+
 const Button = ({
     isDisabled,
     btnType, containerStyle, textStyle, title, rightIcon, handleClick
