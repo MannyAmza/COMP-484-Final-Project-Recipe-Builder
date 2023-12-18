3dac09687dab0707c84b4445e61a842937fff269
Mansoor Amza <113126176+MannyAmza@users.noreply.github.com>
2023-12-18 15:43:33 -0800

    Update Navbar.jsx

--- recipe-builder/src/components/Navbar.jsx
+++ recipe-builder/src/components/Navbar.jsx
@@ -4,17 +4,24 @@ import { HiMenuAlt3 } from 'react-icons/hi'
 import { AiOutlineClose } from 'react-icons/ai'
 import Button from './Button'
 
+/**
+ * Navbar component for the application.
+ */
 const Navbar = () => {
+    // State to manage the open/closed state of the mobile menu
     const [open, setOpen] = useState(false)
 
     return (
         <header className='w-full fixed z-10 bg-black opacity-90'>
+            {/* Desktop Navigation */}
             <nav className='flex w-full py-2 md:py-3 px-4 md:px-20 items-center justify-between'>
+                {/* Logo and Brand */}
                 <a href="/" className='flex items-center justify-center text-white text-lg cursor-pointer'>
                     <img src={Logo} alt="Logo" className='hidden md:block w-8 h-8 lg:w-14 lg:h-14' />
                     Cook<span className="text-orange-500 text-xl">Therapy</span>
                 </a>
 
+                {/* Desktop Navigation Links */}
                 <ul className='hidden md:flex text-orange-500 gap-32'>
                     <li>
                         <a href="/">Home</a>
@@ -27,17 +34,21 @@ const Navbar = () => {
                     </li>
                 </ul>
 
+                {/* Sign In Button */}
                 <Button
                     title='Sign in'
                     conteinerStyle='hidden md:block bg-transparent border border-white text-white hover:bg-white hover:text-slate-700 rounded-full min-w-[130px]'
                 />
 
+                {/* Mobile Menu Toggle Button */}
                 <button className='block md:hidden text-white text-xl'
                     //On click change value of the opening state.
                     onClick={() => setOpen(prev => !prev)}>
                     {open ? <AiOutlineClose /> : <HiMenuAlt3 />}
                 </button>
             </nav>
+            
+            {/* Mobile Navigation Menu */}
             <div className={`${open ? "flex" : "hidden"} bg-black flex-col w-full px-4 pt-16 pb-10 text-white gap-6 text-[14px]`}>
                 <a href="/">Home</a>
                 <a href="/#recipes">Recipes</a>
