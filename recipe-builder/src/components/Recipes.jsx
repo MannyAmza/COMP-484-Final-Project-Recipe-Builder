dbdd0c82e512f6cd14d2500eae3972f8312a1de6
Mansoor Amza <113126176+MannyAmza@users.noreply.github.com>
2023-12-18 16:12:34 -0800

    Update Recipes.jsx

--- recipe-builder/src/components/Recipes.jsx
+++ recipe-builder/src/components/Recipes.jsx
@@ -6,16 +6,22 @@ import RecipeCard from './RecipeCard'
 import { fetchRecipes } from '../utils'
 import Button from './Button'
 
+/**
+ * Recipes component for displaying and searching recipes.
+ */
 const Recipes = () => {
+    // State to manage the recipes list, search query, limit, and loading state
     const [recipes, setRecipes] = useState([])
     const [query, setQuery] = useState('Vegan')
     const [limit, setLimit] = useState(30)
     const [loading, setLoading] = useState(false)
 
+    // Function to handle input change in the search bar
     const handleChange = (e) => {
         setQuery(e.target.value)
     }
 
+    // Function to fetch recipes based on the current query and limit
     const fetchRecipe = async () => {
         try {
             const data = await fetchRecipes({ query, limit })
@@ -30,16 +36,19 @@ const Recipes = () => {
         }
     }
 
+    // Function to handle the search button click
     const handleSearchedRecipe = async (e) => {
         e.preventDefault()
         fetchRecipe()
     }
 
+    // Function to load more recipes
     const showMore = () => {
         setLimit(prev => prev + 10)
         fetchRecipe()
     }
 
+    // Effect to fetch recipes on component mount
     useEffect(() => {
         setLoading(true)
 
@@ -47,6 +56,7 @@ const Recipes = () => {
 
     }, [])
 
+    // Display loading spinner while recipes are being fetched
     if (loading) {
         return (
             <Loading />
@@ -54,8 +64,10 @@ const Recipes = () => {
     }
     return (
         <div className='w-full'>
+            {/* Search Bar Section */}
             <div className='w-full flex items-center justify-center pt-10 pb-5 px-0 md:px-10'>
                 <form className='w-full lg:w-2/4' onSubmit={handleSearchedRecipe}>
+                    {/* Search Bar Component */}
                     <Searchbar placeholder="eg. Cake, Vegan, Chicken"
                         handleInputChange={handleChange}
                         rightIcon={
@@ -66,9 +78,11 @@ const Recipes = () => {
 
             </div>
 
+            {/* Recipe Display Section */}
             {
                 recipes?.length > 0 ? (
                     <>
+                        {/* Recipe Cards */}
                         <div className='w-full  flex flex-wrap gap-10 px-0 lg:px-10 py-10'>
                             {
                                 recipes?.map((item, index) => (
@@ -76,6 +90,7 @@ const Recipes = () => {
                             }
                         </div>
 
+                        {/* "Show More" Button */}
                         <div className='flex w-full items-center justify-center py-10'>
 
                             <Button
@@ -85,7 +100,8 @@ const Recipes = () => {
                             />
                         </div>
                     </>
-                ) : <div className='text-orange-500 w-full items-center justify-center py-10'>
+                ) : // Displayed when no recipes are found
+                <div className='text-orange-500 w-full items-center justify-center py-10'>
                     <p className='text-center'>No Recipe Found</p>
                 </div>
             }
