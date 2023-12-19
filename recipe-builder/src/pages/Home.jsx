1657f5d9285eac414d6f47be708b51e4b8a3d067
Mansoor Amza <113126176+MannyAmza@users.noreply.github.com>
2023-12-18 16:15:32 -0800

    Update Home.jsx

--- recipe-builder/src/pages/Home.jsx
+++ recipe-builder/src/pages/Home.jsx
@@ -2,17 +2,25 @@ import React from 'react'
 import Header from '../components/Header'
 import Recipes from '../components/Recipes'
 
+/**
+ * Home page component containing the main content for the application.
+ */
 const Home = () => {
   return (
+    // Main container for the home page
     <main className='w-full flex flex-col'>
+      {/* Header Section */}
       <Header
         title={
+          // Custom title element for the header
           <p>CookTherapy!</p>
         }
-        type='home'
+        type='home' // Indicating the type of header (home)
       />
 
+      {/* Recipes Section */}
       <section id="recipes" className='md:max-w-[1440px] mx-auto px-4 md:px-20'>
+        {/* Recipes Component */}
         <Recipes />
       </section>
     </main>
